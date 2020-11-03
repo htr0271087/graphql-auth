dc31ac5676fa0f828fc69ae2ca85f02a219e13ad
Linh Tran <htr0271087@gmail.com>
2020-11-03 17:08:43 +0700

    working on loging

---
+++ client/components/LoginForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h3>Login</h3>
+                <AuthForm />
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
