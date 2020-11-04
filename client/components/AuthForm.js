7bb82f89298fb902c6c43b36c61ea92e4bf51fcc
Linh Tran <htr0271087@gmail.com>
2020-11-04 16:22:01 +0700

    finish auth app

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -6,17 +6,24 @@ class AuthForm extends Component {
         super(props);
         this.state = { email: '', password: '' }
     }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+    }
+
     render() {
         return (
             <div className="row">
-                <form className="col s6">
+                <form className="col s6" onSubmit={this.onSubmit.bind(this)}>
                     <div className="input-field">
-                        <label>Email</label>
-                        <input value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+                        <input placeholder="Email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
                     </div>
                     <div className="input-field">
-                        <label>Password</label>
-                        <input value={this.state.password} onChange={e => this.setState({ password: e.target.value })} />
+                        <input placeholder="Password" type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })} />
+                    </div>
+                    <div className="errors">
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
                     </div>
                     <button className="btn">Submit</button>
                 </form>
--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import mutation from "../mutations/Login";
-import query from "../queries/CurrentUser";
+import mutation from "../mutations/Signup";
 import AuthForm from './AuthForm';
+import query from "../queries/CurrentUser";
 import { hashHistory } from "react-router";
 
-class LoginForm extends Component {
+class SignupForm extends Component {
     constructor(props) {
         super(props);
         this.state = { errors: [] };
@@ -19,21 +19,17 @@ class LoginForm extends Component {
 
     onSubmit({ email, password }) {
         this.props.mutate({
-            variables: {
-                email,
-                password
-            },
+            variables: { email, password },
             refetchQueries: [{ query }]
         }).catch(res => {
             const errors = res.graphQLErrors.map(error => error.message);
             this.setState({ errors });
         });
     }
-
     render() {
         return (
             <div className="container">
-                <h3>Login</h3>
+                <h3>Sign Up</h3>
                 <AuthForm errors={this.state.errors}
                     onSubmit={this.onSubmit.bind(this)} />
             </div>
@@ -42,5 +38,5 @@ class LoginForm extends Component {
 }
 
 export default graphql(query)(
-    graphql(mutation)(LoginForm)
+    graphql(mutation)(SignupForm)
 );
